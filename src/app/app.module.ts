f9b32ed2fd1a58599988394dec11a31f29ead275
Felipe Fernandes Diogo <felipe.diogo@ilegra.com>
2017-04-26 15:00:49 -0300

    adding first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GameControlComponent } from './components/game-control/game-control.component';
+import { EvenComponent } from './components/even/even.component';
+import { OddComponent } from './components/odd/odd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameControlComponent,
+    EvenComponent,
+    OddComponent
   ],
   imports: [
     BrowserModule,
